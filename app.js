85e5c6cb34f6073c7cdbcd67304d8c595d1ab048
MohamedHesham1 <mohamed.hesham.2049@gmail.com>
2022-03-18 08:43:28 +0200

    added delete button

--- app.js
+++ app.js
@@ -3,90 +3,99 @@ let filmData = [
   {
     id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "2",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "3",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "4",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "5",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "6",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "7",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "8",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
   {
-    id: "9",
+    id: "1",
     poster: "./imgs/Dr-Strangelove.jpg",
-    title: "Dr.Strangelove",
+    title: " 1",
     date: "1964",
     rate: "9",
     review:
       "Absolutely outstanding farce. Like a slow-motion screwball. George C. Scott and his faces absolutely run away with this thing.",
   },
 ];
-
-filmData.forEach((filmData) => {
-  CardsContainer.innerHTML += `
-  <div class="card">
-    <img class="card-poster" src=${filmData.poster}  alt="Dr.Strangelove poster" >
+document.addEventListener("DOMContentLoaded", () => {
+  addMovies();
+  deleteCard();
+  addFilm();
+});
+// injecting html card template into the cards container
+function addMovies() {
+  filmData.forEach((filmData) => {
+    CardsContainer.innerHTML += `
+  <li class="card" data-id="${filmData.id}" >
+    <img class="card-poster" src=${filmData.poster}  alt="Dr.Strangelove poster" >  
+    <button class="x-button">
+      <svg class="x-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(174, 254, 255, 1);transform: ;msFilter:;"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+    </button>
     <div class="film-info">
       <h3 class="film-name">${filmData.title}</h3>
       <div class="stats">
@@ -100,6 +109,32 @@ filmData.forEach((filmData) => {
       </div>
       <button class="review-button">view full review</button>
     </div>
-  </div>
+  </li>
   `;
-});
+  });
+  console.log(filmData);
+}
+
+//remove the card from the DOM
+function deleteCard() {
+  CardsContainer.addEventListener("click", (e) => {
+    if (e.target.closest(".x-button")) {
+      e.target.parentElement.remove();
+      deleteFromArray(e);
+    }
+  });
+}
+
+function deleteFromArray(e) {
+  const id = e.target.parentElement.getAttribute("data-id");
+  const filmIndex = filmData.findIndex((film) => film.id == id);
+  filmData.splice(filmIndex, 1);
+  console.log(filmData);
+}
+
+function getFilmInput() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+}
